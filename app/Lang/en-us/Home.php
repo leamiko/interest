41b477b5d5985fe2457990b45814a6f6a9ed48a7
zonkee <zonkee.l@outwitinc.com>
2014-11-10 10:48:25 +0800

    remove multilanguage

--- app/Lang/en-us/Home.php
+++ app/Lang/en-us/Home.php
@@ -1,10 +1,15 @@
 <?php
-
 return array(
-    '_HOME_PAGE_' => 'Home Page',
-    '_SERVICE_' => 'Service',
-    '_CASE_' => 'Case',
-    '_COOPERATION_' => 'Cooperation',
-    '_ABOUT_US_' => 'About Us',
-    '_NEWS_' => 'News'
+    // '_HOME_PAGE_' => 'Home Page',
+    // '_SERVICE_' => 'Service',
+    // '_CASE_' => 'Case',
+    // '_COOPERATION_' => 'Cooperation',
+    // '_ABOUT_US_' => 'About Us',
+    // '_NEWS_' => 'News'
+    '_HOME_PAGE_' => '网站首页',
+    '_SERVICE_' => '服务中心',
+    '_CASE_' => '案例中心',
+    '_COOPERATION_' => '申请合作',
+    '_ABOUT_US_' => '关于我们',
+    '_NEWS_' => '公司动态'
 );
